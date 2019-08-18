1d3f80843cf7c649d685ec0b1cba2484353d7d09
Nathan Ringo <remexre@gmail.com>
2019-08-18 13:04:05 -0500

    ...

--- bootstrapper/src/lang.lisp
+++ bootstrapper/src/lang.lisp
@@ -9,10 +9,17 @@
   (named-lambda append (&rest args)
     (cond ((null args)       nil)
           ((null (cdr args)) (car args))
-          (t (append-2 (car args) (apply-1 (get-function 'append) (cdr args)))))))
+          (t                 (append-2 (car args)
+                                       (apply-1 (get-function 'append) (cdr args)))))))
+
+(set-function 'cons-arg-list
+  (named-lambda cons-arg-list (lst)
+    (cond ((null lst)       nil)
+          ((null (cdr lst)) (car lst))
+          (t                (cons (car lst) (cons-arg-list (cdr lst)))))))
 
 (set-function 'apply
   (named-lambda apply (func &rest args)
-    (apply-1 func (apply-1 (get-function 'append) args))))
+    (apply-1 func (cons-arg-list args))))
 
 ; (in-package 'user)
