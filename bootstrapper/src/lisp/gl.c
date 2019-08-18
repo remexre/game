4a5f8f46bd6fa4c641622079e5f0f7ce146bc9c9
Nathan Ringo <remexre@gmail.com>
2019-08-18 17:58:11 -0500

    ...

---
+++ bootstrapper/src/lisp/gl.c
@@ -0,0 +1,6 @@
+#include "prims.h"
+#include "../io.h"
+#include "args.h"
+#include "eval.h"
+#include "../common.h"
+
---
+++ bootstrapper/src/lisp/gl.h
@@ -0,0 +1,21 @@
+#ifndef GAME_LISP_PRIMS_H
+#define GAME_LISP_PRIMS_H 1
+
+#include "value.h"
+#include "../util.h"
+
+native_func(gl_get_context);
+native_func(gl_get_constant);
+
+native_func(gl_compile_shader);
+native_func(gl_get_shader);
+native_func(gl_attach_shader);
+native_func(gl_detach_shader);
+native_func(gl_delete_shader);
+
+native_func(gl_create_program);
+native_func(gl_delete_program);
+native_func(gl_link_program);
+native_func(gl_use_program);
+
+#endif
